5f0716472f48bb12e2ebea5cab844192d55790d4
Thomas Proschinger <thomas.proschinger@gmail.com>
2020-11-21 22:57:04 +0100

    cleanup init-json

--- init-json.el
+++ init-json.el
@@ -1,7 +1,3 @@
-;; JSON
-;;
-;; Without json-mode, Emacs uses js-mode for .json files.
-
 (use-package json-reformat
   :ensure t
   :pin melpa-stable)
@@ -10,18 +6,13 @@
   :ensure t
   :pin melpa-stable)
 
-;; melpa-stable because the version in gnu is old
 (use-package json-mode
   :ensure t
-
   :pin melpa-stable
-
   :init
-
   (defun my-json-mode-hook ()
     (progn
       (setq-local flycheck-check-syntax-automatically '(save mode-enabled))
       (flycheck-mode +1)))
-
   :hook
   (json-mode . my-json-mode-hook))
