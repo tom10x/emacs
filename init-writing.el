458f27d6262f7d58777dbfa09b945235456240a7
Thomas Proschinger <thomas.proschinger@gmail.com>
2020-11-21 22:29:26 +0100

    cleanup init-writing

--- init-writing.el
+++ init-writing.el
@@ -1,12 +1,16 @@
 (use-package olivetti
   :ensure t
-  :pin melpa-stable ;; obsolete in gnu
+  :pin melpa-stable
   :delight olivetti-mode)
 
 (define-derived-mode my-write-mode org-mode "my-write"
   "Major mode for writing."
   (progn
     (setq-local org-startup-indented nil)
-    (company-mode -1)
-    (local-set-key (kbd "\"") (lambda () (interactive) (insert "“”")))
+    (company-mode -1)                     ; company distracts
+    (local-set-key
+     (kbd "\"")
+     (lambda ()
+       (interactive)
+       (insert "“”")))
     (olivetti-mode t)))
