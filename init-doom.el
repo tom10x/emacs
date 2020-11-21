987020efd14cffed75a1f3d8b04ca4d937180746
Thomas Proschinger <thomas.proschinger@gmail.com>
2020-11-21 22:55:55 +0100

    cleanup init-doom

--- init-doom.el
+++ init-doom.el
@@ -1,37 +1,26 @@
 (use-package doom-themes
   :ensure t
-
   :pin melpa-stable
-
   :after (ivy)
-
   :config
-
   (load-theme 'doom-dracula t)
   (set-face-attribute 'org-level-1 nil :height 1.0)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-
-  ;; tweak ivy for dracula
-  (custom-set-faces
+  (doom-themes-visual-bell-config)         ; flash modeline on error
+  (doom-themes-org-config)                 ; tweak org-mode
+  (custom-set-faces                        ; tweak ivy for theme
    '(ivy-current-match
      ((((class color) (background light))
        :background "#ffff44" :foreground "#000000")
       (((class color) (background dark))
-       :background "#ffff44" :foreground "#000000"))))
-
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+       :background "#ffff44" :foreground "#000000")))))
 
 (use-package doom-modeline
   :ensure t
-
   :pin melpa-stable
-
   :init
-  (setq doom-modeline-minor-modes t) ;; otherwise Projectile info won't show
-  (setq doom-modeline-env-version nil) ;; saves some space
-  (setq doom-modeline-buffer-encoding nil)
-
-  :hook (after-init . doom-modeline-mode))
+  (setq
+   doom-modeline-minor-modes t        ; show Projectile info
+   doom-modeline-env-version nil      ; save space
+   doom-modeline-buffer-encoding nil) ; save space
+  :hook
+  (after-init . doom-modeline-mode))
