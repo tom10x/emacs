45713ea1e607df82997a1a8e6389d9c9e216e38a
Thomas Proschinger <thomas.proschinger@gmail.com>
2020-11-23 16:00:09 +0100

    cleanup init-spelling

--- init-spelling.el
+++ init-spelling.el
@@ -1,6 +1,12 @@
-(defvar my-spelling-default-dictionary "en_US")
+(defvar
+  my-spelling-default-dictionary
+  "en_US"
+  "The default dictionary to use, default is en_US = American English.")
 
-(defvar my-spelling-binary "hunspell")
+(defvar
+  my-spelling-binary
+  "hunspell"
+  "Binary to use, specify full path if its not on PATH.")
 
 (use-package ispell
   :ensure nil
