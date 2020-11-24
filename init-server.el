e078e9c6a819fe92b855d9f96cf4d92f8960641f
Thomas Proschinger <thomas.proschinger@gmail.com>
2020-11-24 18:08:51 +0100

    feedback on server start and restart

--- init-server.el
+++ init-server.el
@@ -7,18 +7,23 @@
   :init
   (defun my-server-start ()
     "Start Emacs server."
-    (progn
-      (unless (server-running-p)
+    (if (server-running-p)
+        (message "Server can not start since it's already running. Consider restarting it.")
+      (progn
         (server-start)
         (remove-hook                            ; Kill buffer without asking, even if
          'kill-buffer-query-functions           ; there are connected clients.
-         'server-kill-buffer-query-function))))
+         'server-kill-buffer-query-function)
+        (message "Server started.")
+        )))
+
   (defun my-server-restart ()
     "Restart Emacs server."
     (interactive)
     (progn
       (server-force-delete)
-      (my-server-start)))
+      (my-server-start)
+      (message "Server restarted")))
   :config
   (add-hook 'server-switch-hook #'raise-frame)   ; bring Emacs to front when running emacsclient cmd
   (add-hook 'after-init-hook #'my-server-start)) ; run server after init
