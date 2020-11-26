7182e400f5723495ed2d491db36ac56796a091e0
Thomas Proschinger <thomas.proschinger@gmail.com>
2020-11-26 19:28:43 +0100

    skip specifying dependencies

--- init-magit.el
+++ init-magit.el
@@ -1,15 +1,3 @@
-(use-package ghub
-  :ensure t
-  :pin melpa-stable)
-
-(use-package magit-popup
-  :ensure t
-  :pin melpa-stable)
-
-(use-package git-commit
-  :ensure t
-  :pin melpa-stable)
-
 (use-package magit
   :ensure t
   :pin melpa-stable)
